48c85c93342b602bd8d1f57eb5a31d8a3b63c038
7ODAXXX <mh9413169@gmail.com>
2026-03-28 18:06:02 +0200

    cluster.py

--- cluster.py
+++ cluster.py
@@ -1,12 +1,12 @@
 import sys
 import pandas as pd
+import subprocess
 from sklearn.cluster import KMeans
 
 file_path = sys.argv[1]
 df = pd.read_csv(file_path)
 
 df.columns = df.columns.str.strip().str.lower()
-
 
 cols = [
     'number_of_persons_injured',
@@ -20,10 +20,13 @@ data = df[cols].fillna(0)
 kmeans = KMeans(n_clusters=3, random_state=42)
 df['cluster'] = kmeans.fit_predict(data)
 
-output_path = "clustered_data.csv"
-df.to_csv(output_path, index=False)
+counts = df['cluster'].value_counts().sort_index()
 
-print("Clustering done")
+with open("clusters.txt", "w") as f:
+    for cluster_id, count in counts.items():
+        f.write(f"Cluster {cluster_id}: {count} samples\n")
+
+print("Clusters saved")
 
 # call next script
-subprocess.run(["python", "summary.sh", output_path])
+subprocess.run(["bash", "summary.sh", "clusters.txt"])
